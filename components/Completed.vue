c3888992c379c9cc7ab8fdffe61d6524af600e38
Reynold Osei Adade <reynold@walulel.com>
2021-10-26 11:33:16 +0000

    removed todo schema

---
+++ components/Completed.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint fugiat
+    maxime, quibusdam modi reprehenderit quisquam distinctio illo unde
+    consequatur nihil, fuga vel provident! Tempora facilis tempore, eos vel nemo
+    laboriosam?
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,61 @@
 <template>
-  <Tutorial/>
+  <div class="w-full p-10 flex flex-col justify-center items-center">
+    <div class="flex w-3/4">
+      <button
+        v-for="button in buttons"
+        :key="button.name"
+        class="flex-1 p-2 border"
+        :class="{
+          'bg-white text-blue-500': button.component !== currentComponent,
+          'bg-blue-500 text-white': button.component === currentComponent,
+        }"
+        @click.prevent="setCurrentComponent(button.component)"
+      >
+        {{ button.name }}
+      </button>
+    </div>
+    <div class="w-3/4">
+      <Component :is="currentComponent" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import Pending from '@/components/Pending.vue'
+export default {
+  components: {
+    Pending,
+  },
+  data() {
+    return {
+      task: '',
+      tasks: [],
+      currentComponent: 'Pending',
+      buttons: [
+        { name: 'Pending', component: 'Pending' },
+        { name: 'Completed', component: 'Completed' },
+        { name: 'Deleted', component: 'Deleted' },
+      ],
+    }
+  },
+  methods: {
+    setCurrentComponent(component) {
+      this.currentComponent = component
+    },
+    addTask(task) {
+      // use date as id
+      const newTask = { name: task, id: Date.now(), status: 'pending' }
+      this.tasks = [...this.tasks, newTask]
+    },
+    removeTask(task) {
+      // filter by id
+      this.tasks = this.tasks.filter((item) => item.id !== task.id)
+    },
+    markAsCompleted(task) {
+      const completedTask = { ...task, completed: true }
+      const filteredTasks = this.tasks.filter((item) => item.id !== task.id)
+      this.tasks = [...filteredTasks, completedTask]
+    },
+  },
+}
 </script>
